bcb73df92e4b7a5cdc4a179d7f2ef4d4c80d38f2
Sanjeevikumar M <sanjeevikumar2225@gmail.com>
2026-06-30 12:01:13 +0530

    Updated Schemas

--- vehicle-scheduler-be/app/schemas/schedule.py
+++ vehicle-scheduler-be/app/schemas/schedule.py
@@ -1,1 +1,31 @@
- 
+from pydantic import BaseModel
+from typing import List
+
+class Depot(BaseModel):
+    ID: int
+    MachanicHours: int
+
+class Vehicle(BaseModel):
+    TaskID: str
+    Duration: int
+    Impact: int 
+
+class DepotListResponse(BaseModel):
+    depots: List[Depot]
+
+class VehicleListResponse(BaseModel):
+    vehicles: List[Vehicle]
+
+class ScheduleDepotList(BaseModel):
+    depot_id: int
+    machanic_hours_budget: int
+    hours_used: int
+    total_impact_score: int
+    scheduled_task: List[str]
+
+class ScheduleSummaryResponse(BaseModel):
+    total_depots: int
+    total_allocated_hours: int
+    total_impact_score: int
+    results: List[ScheduleDepotList]
+    
